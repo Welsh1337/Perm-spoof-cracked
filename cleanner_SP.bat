05ee1994505857e4e469467059ff8fe52901d03f
Welsh <53702649+Welsh1337@users.noreply.github.com>
2022-04-04 09:17:38 -0700

    Add files via upload

---
+++ cleanner_SP.bat
@@ -0,0 +1,7 @@
+@echo off
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\DigitalEntitlements
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\data\server-cache
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\data\cache
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\logs
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\crashes
